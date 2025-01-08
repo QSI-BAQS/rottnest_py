16b8174fc41dffb1546195900a11f1a271b0c221
Alan Robertson <alan@qrop.xyz>
2025-01-08 16:45:39 +1100

    Example architecture controller

--- src/rottnest/server/controller/architecture.py
+++ src/rottnest/server/controller/architecture.py
@@ -1,11 +1,12 @@
 from bottle import request, abort 
 from geventwebsocket import WebSocketError
 
-
+from rottnest.server.model import architecture 
 
 def register_routes(app):
    app.route("/websocket", callback=handle_websocket) 
 
+# TODO: Register architecture object
 def handle_websocket():
     wsock = request.environ.get('wsgi.websocket')
     if not wsock:
@@ -14,7 +15,13 @@ def handle_websocket():
     while True:
         try:
             message = wsock.receive()
-            wsock.send("Your message was: %r" % message)
+            result = socket_binds.get(message, f"Error: {message} not recognised") 
+            wsock.send(result)
         except WebSocketError:
             break
 
+def get_subtype():
+    return json.dumps(architecture.get_subtype())
+
+# Socket commands
+socket_binds = {'subtype': get_subtype} 
