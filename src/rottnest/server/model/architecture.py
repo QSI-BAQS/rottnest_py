c8cef6d1b8950701d7052c784da1a605117ce2ae
Alan Robertson <alan@qrop.xyz>
2025-01-08 16:46:59 +1100

    Architecture model

---
+++ src/rottnest/server/model/architecture.py
@@ -0,0 +1,14 @@
+import json
+
+# from t_scheduler import regions
+
+def get_subtypes(): 
+    ...
+    # Import architecture, get subtype names
+    return {
+'register': ['Comb', 'Linear'],
+'bus': ['Edge Disjoint'],
+'t_factory': ['3x5', '4x6', 'Cultivator']
+}
+ 
+    
