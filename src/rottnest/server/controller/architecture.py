807d3cd93fd83a40208febcd59814cb426f544de
ahto <ahto@artifact8.xyz>
2025-01-10 20:43:03 +1100

    some small hacks to get some messages looking cleaner

--- src/rottnest/server/controller/architecture.py
+++ src/rottnest/server/controller/architecture.py
@@ -1,10 +1,12 @@
 from bottle import request, abort 
 from geventwebsocket import WebSocketError
-
+from rottnest.region_builder import json_to_region
 from rottnest.server.model import architecture 
 
+import json
+
 def register_routes(app):
-   app.route("/websocket", callback=handle_websocket) 
+   app.route("/websocket", callback=handle_websocket)
 
 # TODO: Register architecture object
 def handle_websocket():
@@ -25,7 +27,19 @@ def err(message, *args, **kwargs):
     return f"Error: {message} not recognised"
 
 def get_subtype(*args, **kwargs):
-    return json.dumps(architecture.get_subtype())
+    return json.dumps({
+        'message': 'subtype',
+        'subtypes': architecture.get_region_subtypes()
+    })
 
+
+def use_arch(*args, **kwargs):
+    return json.dumps({
+        'message': 'usearch',
+        'payload': json_to_region.example
+    })
 # Socket commands
-socket_binds = {'subtype': get_subtype} 
+socket_binds = {
+        'subtype': get_subtype,
+        'usearch': use_arch
+        } 
