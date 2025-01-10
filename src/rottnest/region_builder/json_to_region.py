cd3ee861980e5f9cca3e2844afe55d8e155fdb40
Alan Robertson <alan@qrop.xyz>
2025-01-10 16:41:55 +1100

    Added example json

--- src/rottnest/region_builder/json_to_region.py
+++ src/rottnest/region_builder/json_to_region.py
@@ -5,49 +5,48 @@ def conv(region_type_str, width, height, *args, **kwargs) -> callable:
     return partial(region_types[region_type_str], *args, width=width, height=height, **kwargs)
 
 
-
 example = {
-height: 20
-width: 10
-regions:
+"height": 20,
+"width": 10,
+"regions":
 [
 {
-    region_type : "CombShapedRegion",
-    x: 0,
-    y: 0,
-    height: 10,
-    width: 10,
-    downstream: [
+    "region_type": "CombShapedRegion",
+    "x": 0,
+    "y": 0,
+    "height": 10,
+    "width": 10,
+    "downstream": [
     {
-        region_type: "RouteBus",
-        x: 0,
-        y: 10,
-        height: 1,
-        width: 10,
-        downstream: [
+        "region_type": "RouteBus",
+        "x": 0,
+        "y": 10,
+        "height": 1,
+        "width": 10,
+        "downstream": [
             {
-                region_type: "BellRegion", 
-                x: 0,
-                y: 11, 
-                height: 9,
-                width: 1,
-                bell_rate: 1
+                "region_type": "BellRegion", 
+                "x": 0,
+                "y": 11, 
+                "height": 9,
+                "width": 1,
+                "bell_rate": 1
             }, 
             {
-                region_type: "MagicStateFactoryRegion",
-                x: 1,
-                y: 11, 
-                height: 9,
-                width: 8,
-                factory_type: "cultivator" 
+                "region_type": "MagicStateFactoryRegion",
+                "x": 1,
+                "y": 11, 
+                "height": 9,
+                "width": 8,
+                "factory_type": "cultivator" 
             },
             {
-                region_type: "BellRegion", 
-                x: 9,
-                y: 11, 
-                height: 9,
-                width: 1,
-                bell_rate: 1
+                "region_type": "BellRegion", 
+                "x": 9,
+                "y": 11, 
+                "height": 9,
+                "width": 1,
+                "bell_rate": 1
             },
         ]
     }
