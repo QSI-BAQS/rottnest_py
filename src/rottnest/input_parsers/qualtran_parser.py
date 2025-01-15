a7e1975fa98e17bf9d9a7e7e5bfd4e48c8fbec23
Alan Robertson <alan@qrop.xyz>
2025-01-15 15:59:33 +1100

    Initial qualtran parser pass

---
+++ src/rottnest/input_parsers/qualtran_parser.py
@@ -0,0 +1,35 @@
+from functools import partial 
+
+import qualtran
+import qualtran.bookkeeping
+
+
+def arbitrary_clifford():
+    '''
+        Worst case N qubit clifford
+    '''
+    def _wrap(
+        self,
+        operation_sequence: OperationSequence,
+        qubit_labels: QubitLabelTracker,
+        rz_tags: QubitLabelTracker):
+        
+        # Replace this with an n qubit arb cliff 
+        for i in range(0, self.n, 2): 
+            operation_sequence.append(
+                cabaliser.gates.CNOT,
+                (0, 1) # Get targets
+            )
+
+def blank():
+    def _wrap():
+        pass
+    return _wrap
+
+
+qualtran_ops = {
+    bookkeeping.join.Join: blank,
+    bookkeeping.split.Split: blank,
+    bookkeeping.arbitrary_clifford.ArbitraryClifford: arbitrary_clifford
+}
+
