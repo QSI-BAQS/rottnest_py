4143b9a67b93bf2133d06aeae88655cd0eadc7fd
jqxcz <jqxczz@gmail.com>
2025-01-22 16:53:18 +1100

    Rename t_scheduler.widget to t_scheduler.region

--- src/rottnest/server/model/architecture.py
+++ src/rottnest/server/model/architecture.py
@@ -3,8 +3,8 @@ import random
 from typing import Any
 
 # Ill advised, but forces the generation and capture of the region types
-from t_scheduler.widget import * 
-from t_scheduler.widget.region_types import region_types, region_args
+from t_scheduler.region import * 
+from t_scheduler.region.region_types import region_types, region_args
 
 from t_scheduler.router import *
 from t_scheduler.router import region_router_exports
