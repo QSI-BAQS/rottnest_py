fac9f42f1d25d00c39bd96bebf543cadd6823e45
jqxcz <jqxczz@gmail.com>
2025-01-25 00:37:16 +1100

    Add more default routers TODO use tyson field

--- src/rottnest/region_builder/json_to_region.py
+++ src/rottnest/region_builder/json_to_region.py
@@ -23,6 +23,7 @@ default_router = {
     'TCultivatorBufferRegion.with_dense_layout': DenseTCultivatorBufferRouter,
     'MagicStateFactoryRegion': MagicStateFactoryRouter,
     'MagicStateFactoryRegion.with_litinski_6x3_dense': MagicStateFactoryRouter,
+    'MagicStateFactoryRegion.with_litinski_5x3': MagicStateFactoryRouter,
     'MagicStateBufferRegion': RechargableBufferRouter,
     'SingleRowRegisterRegion': BaselineRegisterRouter,
     'PrefilledMagicStateRegion': VerticalFilledBufferRouter,
