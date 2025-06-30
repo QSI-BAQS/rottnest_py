aa0f427732c01b9f71b46cff0aaddf3c522c825f
ahtoms <ahto@artifact8.xyz>
2025-06-30 21:08:12 +1000

    almost forgot about this

--- src/rottnest/plugin/loader.py
+++ src/rottnest/plugin/loader.py
@@ -2,14 +2,23 @@
 import json
 import importlib.util
 from enum import Enum
+from api_defaults import default_api_map
 
 class ArchLocationKind(Enum):
+    '''
+       Location Kind, it outlines what kind of
+       plugin it is and how that it is held within rottnest 
+    '''
     FilePath = 1
     ModuleKey = 2
 
 
 class ArchConfigEntry:
-
+    '''
+       Configuration entry that is used to describe the plugin
+       The expectation is that it will be loaded as a json
+       object
+    '''
     def __init__(self, name, location, kind):
         '''
            Configuration entry, is a reference to where
@@ -42,27 +51,30 @@ class ArchConfigEntry:
     def load_arch(self):
         '''
            Loads an architecture that it is currently
-           referring to. It will invoke 'load_arch' 
+           referring to. It will invoke 'load_arch'
+
+           This will return an `ArchPlugin` object or None
         '''
         if self.kind == ArchLocationKind.FilePath:
-            with open(self.location, 'r') as cfg:
-
-                contents = 
-        elif self.kind == ArchLocatioNKind.ModuleKey:
-            mod = importlib.
-            
+            return ArchPluginMap.load_plugin_from_file(self.name, self.location)
+        elif self.kind == ArchLocationKind.ModuleKey:
+            return ArchPluginMap.load_plugin_from_module(self.name, self.location)
         else:
             None
 
         
-class ArchConfig:
-
-    def __init__(self, path):
+class ArchRegistryConfig:
+    '''
+       Configuration for an arch registry
+    '''
+    def __init__(self, path, entries=[]):
         '''
-           Constructs an arch configuration  
+           Constructs an arch configuration
+           path is a filepath
+           entries is a list
         '''
         self.path = path
-        self.entries = {}
+        self.entries = entries
     
     @staticmethod
     def load_config(path):
@@ -75,63 +87,74 @@ class ArchConfig:
             contents = file.read()
             parsed_entries = json.loads(contents)
             for e in parsed_entries.items():
-                location = e['identifier']
+                name = e['identifier']
+                description = e['description']
+                kind = description['kind']
+                location = description['location']
 
-    
+                entries.append(ArchConfigEntry(name, location, kind))
+                
+        config = ArchRegistryConfig(path, entries)
+        return config    
 
 
-class ArchPlugin:
+class ArchPluginMap:
     '''
        Architecture Plugin, holds an interface for
        operations  
     '''
 
-    def __init__(self, identifier):
+    def __init__(self, identifier, plugin_map):
         '''
            Creates a new Plugin that can be used by
            rottnest, this plugin  
         '''
         self.identifier = identifier
         self.api_map = default_api_map(identifier)
-        self.arch_map = {}
+        self.plugin_map = plugin_map
         
 
     @staticmethod
-    def load_plugin_from_file(plugin_name, filepath):
+    def load_plugin_map_from_file(plugin_name, filepath):
         '''
            Loads a python module from file 
            Calls `all_architectures()` and registers them
         '''
-        archplug = ArchPlugin(plugin_name)
         spec = importlib.util.spec_from_file_location(plugin_name, filepath)    
         plugin_obj = importlib.util.module_from_spec(spec)
         # It is not known what function to call
         
         
-        return ArchPlugin.retrieve_plugin(plugin_obj)
+        return ArchPluginMap.retrieve_plugin_map(plugin_name, plugin_obj)
 
 
     @staticmethod
-    def load_plugin_from_env(plugin_name, location):
+    def load_plugin_map_from_env(plugin_name, location):
         '''
            Loads a python module from module space
            Calls `all_architectures()` and registers them
         '''
-        archplug = ArchPlugin(plugin_name)
         plugin_obj = importlib.import_module(location)
         # It is not known what function to call
 
-        return ArchPlugin.retrieve_plugin(plugin_obj)
+        return ArchPluginMap.retrieve_plugin_map(plugin_name, plugin_obj)
 
     @staticmethod
-    def retrieve_plugin(mod):
-        return None
+    def retrieve_plugin_map(name, modrep):
+        '''
+           Retrieves the architecture map object
+           and extracts the list of plugins
+        '''
+        archmap = modrep.architectures()
+        return archmap.plugins()
 
         
 
 
 class ArchPluginRegistry:
-
+    '''
+       Registry of architecture factories 
+    '''
     def __init__(self):
         '''
            ArchPluginRegistry, holds a registry of architecture
@@ -152,4 +175,27 @@ class ArchPluginRegistry:
         '''
         return self.arch_map[name]
 
-    
+    @staticmethod
+    def from_plugin_map(plug_map):
+        '''
+           Constructs a plugin registry with a plugin map 
+        '''
+        reg = ArchPluginRegistry()
+        for p in plug_map:
+            reg.register_plugin(p)
+
+        return reg
+
+
+    @staticmethod
+    def from_plugin_maps(plug_maps):
+        '''
+           Constructs a plugin registry with many plugin maps 
+        '''
+        reg = ArchPluginRegistry()
+        for pm in plug_maps:
+            for p in pm:
+                reg.register_plugin(p)
+
+        return reg
+        
