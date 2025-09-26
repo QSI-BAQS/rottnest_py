9b65dc0a16989b8b14b771d701b39b27a806bf24
Alan Robertson <alan@qrop.xyz>
2025-09-26 14:31:17 +1000

    Testing fh

--- src/rottnest/executables/current_executable.py
+++ src/rottnest/executables/current_executable.py
@@ -1,7 +1,5 @@
-from ejc.executable import EJCSchrodinger, EJCInteraction 
+from rottnest_example_executables import rottnest_executables
 
-current_executable = EJCInteraction(
-    20,
-    4,
-    epsilon_target=0.25,
+current_executable = rottnest_executables[0](
+    N = 5
 )
