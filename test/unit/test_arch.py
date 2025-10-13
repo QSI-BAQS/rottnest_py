ba0ff469e59523d2e2456c6664d0a2bd25392388
Ben Y. <bjy777101@gmail.com>
2025-10-13 12:06:06 +1100

    Added testcases for the various methods of loading a module, and for an architecture loaded from a config

--- test/unit/test_arch.py
+++ test/unit/test_arch.py
@@ -17,9 +17,27 @@ from dummy_arch.dummy_arch import DummyWorker, DummyDesigner, DummyComposer
 
 
 # TODO : Test the loaders used below
+class TestModuleLoading(unittest.TestCase):
+    '''
+        Testcases related to loading modules (providing architectures)
+    '''
+    def testModuleFromString(self):
+        archPlugins = ArchitecturePlugins()
+        module = archPlugins._load_module_from_module_string("dummy_arch")
+        self.assertEqual(len(module.rottnest_architectures), 1)
+
+    def testModuleFromPath(self):
+        archPlugins = ArchitecturePlugins()
+        module = archPlugins._load_module_from_file_path("./standalone.py")
+        self.assertEqual(len(module.rottnest_architectures), 1)
+
+    def testModuleFromConfigFile(self):
+        archPlugins = ArchitecturePlugins()
+        module = archPlugins._load_modules_from_config("./module.conf")[0]
+        self.assertEqual(len(module.rottnest_architectures), 1)
 
 
-class TestDummyModuleStringImportedArchitecture(unittest.TestCase):
+class TestDummyStringImportedArchitecture(unittest.TestCase):
     '''
         Testcases related to the bare minimum inspection of an architecture
         loaded as a plugin via a string
@@ -92,13 +110,13 @@ class TestDummyPathImportedArchitecture(unittest.TestCase):
         self.assertTrue(isinstance(self.arch.composer([ ], [ ]), DummyComposer))
 
 
-class TestDummyStandardImportedArchitecture(unittest.TestCase):
+class TestDummyImportedArchitecture(unittest.TestCase):
     '''
         Testcases matching above, but for an architecture that is exposed by an import
     '''
     def setUp(self):
         import dummy_arch as inner_dummy_arch
-        self.archPlugins = ArchitecturePlugins(modules=[inner_dummy_arch])
+        self.archPlugins = ArchitecturePlugins([inner_dummy_arch])
         self.arch = self.archPlugins['Dummy']
 
     def testArchCount(self):
@@ -123,7 +141,41 @@ class TestDummyStandardImportedArchitecture(unittest.TestCase):
     def testInstantiateComposer(self):
         self.assertTrue(isinstance(self.arch.composer([ ], [ ]), DummyComposer))
 
-# TODO : load from a .conf
+
+class TestDummyConfigImportedArchitecture(unittest.TestCase):
+    '''
+        Testcases matching above, but for an architecture loaded
+        from a path
+    '''
+    def setUp(self):
+        self.archPlugins = ArchitecturePlugins()
+        # Acquire a module and load it into the ArchitecturePlugins
+        module = self.archPlugins._load_modules_from_config("./module.conf")[0]
+        self.archPlugins._modules.add(module)
+        self.archPlugins.load_options_from_modules()
+        self.arch = self.archPlugins['Dummy']
+
+    def testArchCount(self):
+        self.assertEqual(len(self.archPlugins._modules), 1)
+        self.assertEqual(len(self.archPlugins.get_architectures()), 1)
+
+    def testGetName(self):
+        self.assertEqual(self.arch.get_name(), "Dummy")
+
+    def testGetWorkerEntry(self):
+        self.assertTrue(isinstance(self.arch.worker_entrypoint(), Callable))
+
+    def testWorkerEntryMatches(self):
+        self.assertEqual(self.arch.worker_entrypoint(), self.arch.worker.entrypoint)
+
+    def testInstantiateWorker(self):
+        self.assertTrue(isinstance(self.arch.worker(), DummyWorker))
+
+    def testInstantiateDesigner(self):
+        self.assertTrue(isinstance(self.arch.designer(), DummyDesigner))
+
+    def testInstantiateComposer(self):
+        self.assertTrue(isinstance(self.arch.composer([ ], [ ]), DummyComposer))
 
 
 if __name__ == "__main__":
