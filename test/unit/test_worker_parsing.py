13760b2039940da783c76dedb5ea1312ea77e52e
Ben Y. <bjy777101@gmail.com>
2025-10-20 15:39:34 +1100

    Added composition based tests for worker parsing - one currently failing
    Case with 100 qubits (100 X gates) results in 97 gates sequenced only

--- test/unit/test_worker_parsing.py
+++ test/unit/test_worker_parsing.py
@@ -35,35 +35,14 @@ class TestWorkerCircuitCounting(unittest.TestCase):
         the resulting gates
     '''
     def setUp(self):
-        self.n_qubits = 10
-        def count_gates(worker, compute_unit):
-            '''
-                Dummy worker inspects a compute unit
-            '''
-            # Count the gates in the compute_unit
-            #for seq in compute_unit.sequences: print(seq)
-            worker.gate_ctr += sum(len(seq) for seq in compute_unit.sequences)
-
-        # Dummy architecture with;
-        # - Designer memory bound specified above
-        # - Empty dummy composer
-        # - Worker that uses the above `count_gates` method to count gates
-        #   from a compute_unit (uses the `gate_ctr` property to store the count)
-        self.arch = build_arch('CompDummyArch',
-            build_designer('CompDummyDesigner', get_mem_bound=lambda s,l: l['mem_bound']),
-            build_composer('CompDummyComposer'),
-            build_worker('CompDummyWorker', gate_ctr=0, execute_compute_unit=count_gates),
-        )
-
-        # Pretend we actually loaded the architecture (just insert it in for now)
-        architectures._options['CompDummyArch'] = self.arch
-        architectures.set_current_architecture('CompDummyArch')
-
+        self.n_qubits = 100
         # Load n cirq qubits
         self.cirq_qubits = tuple(cirq.NamedQubit(str(x)) for x in range(self.n_qubits))
         # (circuit, n_gates) pairs to check
         # A correct result is when the compute units resulting from the parsing
         # of this circuit have n_gates in total
+
+        # TODO : All gates, high rz that hits memory bound, more qubits
         self.circuits = [
             # --[ Cirq ]--
             (
@@ -124,6 +103,12 @@ class TestWorkerCircuitCounting(unittest.TestCase):
                     cirq.H(self.cirq_qubits[0])
                 ),
                 15
+            ),
+            (
+                cirq.Circuit(
+                    cirq.X(self.cirq_qubits[n]) for n in range(100)
+                ),
+                100
             ),
             # --[ Qualtran ]--
             # Note that the current Qualtran parsing uses the fact that `build_bloq` provides an object whose iterator
@@ -203,14 +188,43 @@ class TestWorkerCircuitCounting(unittest.TestCase):
 
 
     def testCircuitNGates(self):
-        layout_id = 0
+        '''
+            Test a worker that parses a circuit into a compute unit and counts the resulting gates
+            directly
+        '''
+        # Create an architecture where the worker counts gates
+        def count_gates(worker, compute_unit):
+            '''
+                Dummy worker inspects a compute unit
+            '''
+            # Count the gates in the compute_unit
+            n_gates = sum(len(seq) for seq in compute_unit.sequences)
+            worker.gate_ctr += n_gates
+            return n_gates
+
+        # Dummy architecture with;
+        # - Designer memory bound drawn from layout
+        # - Empty dummy composer
+        # - Worker that uses the above `count_gates` method to count gates
+        #   from a compute_unit (uses the `gate_ctr` property to store the count)
+        arch = build_arch('WorkerGateCounting',
+            build_designer('DummyDesigner', get_mem_bound=lambda s,l: l['mem_bound']),
+            build_composer('DummyComposer'),
+            build_worker('FullGateCtrWorker', gate_ctr=0, execute_compute_unit=count_gates),
+        )
+
+        # Pretend we actually loaded the architecture (just insert it in for now)
+        architectures._options['WorkerGateCounting'] = arch
+        architectures.set_current_architecture('WorkerGateCounting')
+
+        layout_id = "circuit_n_gates_layout"
         # Load gates from a sequencer on a cirq object
         for circuit, answer in self.circuits:
             with self.subTest(circuit=circuit, answer=answer):
                 # Instantiate a worker
-                worker = self.arch.worker()
+                worker = arch.worker()
                 # Create dummy layout and load it into the worker
-                dummy_layout = {'mem_bound': 200}
+                dummy_layout = {'mem_bound': 500}
                 worker.load_layout(layout_id, dummy_layout)
 
                 # Sequence the given circuit
@@ -220,6 +234,7 @@ class TestWorkerCircuitCounting(unittest.TestCase):
                 it = seq.sequence_pyliqtr(parser)
 
                 for obj in it:
+                    print(obj)
                     if obj != INTERRUPT:
                         # ^ Ignore cache events
                         # v Pass the sequenced sections of the parsed circuit to the
@@ -227,6 +242,169 @@ class TestWorkerCircuitCounting(unittest.TestCase):
                         worker.execute_compute_unit(obj)
 
                 self.assertEqual(worker.gate_ctr, answer)
+
+
+    def testCircuitNGatesComposed(self):
+        '''
+            Test an architecture where gates are counted by a composer
+        '''
+        def count_gates(worker, compute_unit):
+            '''
+                Dummy worker inspects a compute unit
+            '''
+            # Count the gates in the compute_unit
+            n_gates = sum(len(seq) for seq in compute_unit.sequences)
+            return {'n_gates': n_gates}
+
+        class GateCtrResultsComposer(rottnest_composer.ResultsComposer):
+            def __init__(self, result_dict=None, n_obj=1, comp_unit=None):
+                super().__init__()
+
+                if result_dict is None:
+                    result_dict = {'n_gates': 0}
+
+                self._obj = result_dict
+
+            def __add__(self, other):
+                res = GateCtrResultsComposer(result_dict={'n_gates': self._obj['n_gates'] + other._obj['n_gates']})
+                res._unit_ids = self._unit_ids + other._unit_ids
+                res._n_obj = self._n_obj + other._n_obj
+                return res
+
+            def __iadd__(self, other):
+                self._unit_ids += other._unit_ids
+                self._n_obj += other._n_obj
+                self._obj['n_gates'] += other._obj['n_gates']
+                return self
+
+
+            def get_gate_count(self):
+                return self._obj['n_gates']
+
+
+        arch = build_arch('ComposedGateCounting',
+            build_designer('DummyDesigner', get_mem_bound=lambda s, l : l['mem_bound']),
+            build_composer('GateCtrComposer', results_composer_constructor=lambda s: GateCtrResultsComposer),
+            build_worker('SingleGateCtrWorker', execute_compute_unit=count_gates)
+        )
+
+        # Pretend we actually loaded the architecture (just insert it in for now)
+        architectures._options['ComposedGateCounting'] = arch
+        architectures.set_current_architecture('ComposedGateCounting')
+
+        layout_id = "composed_n_gates_layout"
+        # Load gates from a sequencer on a cirq object
+        for circuit, answer in self.circuits:
+            with self.subTest(circuit=circuit, answer=answer):
+                # Instantiate a worker and composer
+                worker = arch.worker()
+                # Create dummy layout and load it into the worker
+                dummy_layout = {'mem_bound': 500}
+                worker.load_layout(layout_id, dummy_layout)
+
+                # Empty qubit tracker
+                composer = arch.composer([dummy_layout], [])
+
+                # Sequence the given circuit
+                parser = PyliqtrParser(circuit)
+                seq = Sequencer(layout_id)
+                parser.parse()
+                it = seq.sequence_pyliqtr(parser)
+
+                res_composer = composer.results_composer_constructor()()
+
+                for obj in it:
+                    if obj != INTERRUPT:
+                        # ^ Ignore cache events
+                        # v Pass the sequenced sections of the parsed circuit to the
+                        # worker
+                        res = worker.execute_compute_unit(obj)
+                        res_composer += composer.compose_result(obj.unit_id, res)
+
+                self.assertEqual(res_composer.get_gate_count(), answer)
+
+
+    def testCircuitNGatesComposedLowMemory(self):
+        '''
+            Test an architecture where gates are counted by a composer,
+            and there is a sufficiently low memory constraint
+        '''
+        # Set up worker and composer methods
+        def count_gates(worker, compute_unit):
+            '''
+                Dummy worker inspects a compute unit
+            '''
+            # Count the gates in the compute_unit
+            n_gates = sum(len(seq) for seq in compute_unit.sequences)
+            return {'n_gates': n_gates}
+
+        class GateCtrResultsComposer(rottnest_composer.ResultsComposer):
+            def __init__(self, result_dict=None, n_obj=1, comp_unit=None):
+                super().__init__()
+
+                if result_dict is None:
+                    result_dict = {'n_gates': 0}
+
+                self._obj = result_dict
+
+            def __add__(self, other):
+                res = GateCtrResultsComposer(result_dict={'n_gates': self._obj['n_gates'] + other._obj['n_gates']})
+                res._unit_ids = self._unit_ids + other._unit_ids
+                res._n_obj = self._n_obj + other._n_obj
+                return res
+
+            def __iadd__(self, other):
+                self._unit_ids += other._unit_ids
+                self._n_obj += other._n_obj
+                self._obj['n_gates'] += other._obj['n_gates']
+                return self
+
+
+            def get_gate_count(self):
+                return self._obj['n_gates']
+
+
+        arch = build_arch('ComposedGateCountingLowMem',
+            build_designer('DummyDesigner', get_mem_bound=lambda s, l : l['mem_bound']),
+            build_composer('GateCtrComposer', results_composer_constructor=lambda s: GateCtrResultsComposer),
+            build_worker('SingleGateCtrWorker', execute_compute_unit=count_gates)
+        )
+
+        # Pretend we actually loaded the architecture (just insert it in for now)
+        architectures._options['ComposedGateCountingLowMem'] = arch
+        architectures.set_current_architecture('ComposedGateCountingLowMem')
+
+        layout_id = "composed_n_gates_low_mem_layout"
+        # Load gates from a sequencer on a cirq object
+        for circuit, answer in self.circuits:
+            with self.subTest(circuit=circuit, answer=answer):
+                # Instantiate a worker and composer
+                worker = arch.worker()
+                # Create dummy layout and load it into the worker
+                dummy_layout = {'mem_bound': 100}
+                worker.load_layout(layout_id, dummy_layout)
+
+                # Empty qubit tracker
+                composer = arch.composer([dummy_layout], [])
+
+                # Sequence the given circuit
+                parser = PyliqtrParser(circuit)
+                seq = Sequencer(layout_id)
+                parser.parse()
+                it = seq.sequence_pyliqtr(parser)
+
+                res_composer = composer.results_composer_constructor()()
+
+                for obj in it:
+                    if obj != INTERRUPT:
+                        # ^ Ignore cache events
+                        # v Pass the sequenced sections of the parsed circuit to the
+                        # worker
+                        res = worker.execute_compute_unit(obj)
+                        res_composer += composer.compose_result(obj.unit_id, res)
+
+                self.assertEqual(res_composer.get_gate_count(), answer)
+
 
 if __name__ == "__main__":
     unittest.main()
