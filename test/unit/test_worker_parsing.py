2c6a464ea6fe5349a263c6596f542508ad29f49a
Ben Y. <bjy777101@gmail.com>
2025-11-10 10:53:35 +1100

    Refactored test_worker_parsing to better split gate counting and rz collection

--- test/unit/test_worker_parsing.py
+++ test/unit/test_worker_parsing.py
@@ -1,6 +1,9 @@
 '''
     Testcases related to using an architecture to parse
     Cirq/Qualtran circuits
+
+    Tests both a trivial custom gate counting worker and
+    the internal rz_collection worker
 '''
 
 import unittest
@@ -41,86 +44,83 @@ from test_data.test_circuits import cirq_circuits, cirq_qubits, qualtran_circuit
 # if None, uses system time
 RAND_SEED = None
 
+'''
+    Create a selection of useful architectures and their associated methods
+'''
+# -- Direct Counting --
+def count_gates_direct(worker, compute_unit):
+    '''
+        Direct gate counter to be used as a worker's "compilation" method
+    '''
+    # Count the gates in the compute_unit
+    n_gates = reduce(lambda x, y: x + len(y), compute_unit.sequences, 0)
+    worker.gate_ctr += n_gates
+
+
+direct_counter_arch = build_arch('DirectGateCounting',
+    build_designer('DummyDesigner', get_mem_bound=lambda s,l: l['mem_bound']),
+    build_composer('DummyComposer'),
+    build_worker('FullGateCtrWorker', gate_ctr=0, execute_compute_unit=count_gates_direct),
+)
+
+architectures._options['DirectGateCounting'] = direct_counter_arch
+
+# -- Composed Counting --
+def count_gates_composed(worker, compute_unit):
+    '''
+        Gate counter that results in a composable description
+    '''
+    # Count the gates in the compute_unit
+    n_gates = reduce(lambda x, y: x + len(y), compute_unit.sequences, 0)
+    return {'n_gates': n_gates}
+
+class GateCtrResultsComposer(rottnest_composer.ResultsComposer):
+    '''
+        Results composer that combines gate counter descriptions
+    '''
+    def __init__(self, result_dict=None, n_obj=1, comp_unit=None):
+        super().__init__()
+
+        if result_dict is None:
+            result_dict = {'n_gates': 0}
+
+        self._obj = result_dict
+
+    def __add__(self, other):
+        res = GateCtrResultsComposer(result_dict={'n_gates': self._obj['n_gates'] + other._obj['n_gates']})
+        res._unit_ids = self._unit_ids + other._unit_ids
+        res._n_obj = self._n_obj + other._n_obj
+        return res
+
+    def __iadd__(self, other):
+        self._unit_ids += other._unit_ids
+        self._n_obj += other._n_obj
+        self._obj['n_gates'] += other._obj['n_gates']
+        return self
+
+    def get_gate_count(self):
+        return self._obj['n_gates']
+
+composed_counter_arch = build_arch('ComposedGateCounting',
+    build_designer('DummyDesigner', get_mem_bound=lambda s, l : l['mem_bound']),
+    build_composer('GateCtrComposer', results_composer_constructor=lambda s: GateCtrResultsComposer),
+    build_worker('SingleGateCtrWorker', execute_compute_unit=count_gates_composed)
+)
+
+architectures._options['ComposedGateCounting'] = composed_counter_arch
+
+'''
+    Create a selection of useful layouts
+'''
+LayoutProxy.add_layout_with_id("std_layout", {'mem_bound': 500})
+LayoutProxy.add_layout_with_id("low_mem_layout", {'mem_bound': 50})
+
 
 class TestWorkerCircuitCounting(unittest.TestCase):
     '''
         Tests a worker that parses and "compiles" a circuit by counting
         the resulting gates
     '''
-    @classmethod
-    def setUpClass(cls):
-        '''
-            Create a selection of useful architectures and their associated methods
-        '''
-        # -- Direct Counting --
-        def count_gates_direct(worker, compute_unit):
-            '''
-                Direct gate counter to be used as a worker's "compilation" method
-            '''
-            # Count the gates in the compute_unit
-            n_gates = reduce(lambda x, y: x + len(y), compute_unit.sequences, 0)
-            worker.gate_ctr += n_gates
-
-
-        direct_counter_arch = build_arch('DirectGateCounting',
-            build_designer('DummyDesigner', get_mem_bound=lambda s,l: l['mem_bound']),
-            build_composer('DummyComposer'),
-            build_worker('FullGateCtrWorker', gate_ctr=0, execute_compute_unit=count_gates_direct),
-        )
-
-        architectures._options['DirectGateCounting'] = direct_counter_arch
-
-        # -- Composed Counting --
-        def count_gates_composed(worker, compute_unit):
-            '''
-                Gate counter that results in a composable description
-            '''
-            # Count the gates in the compute_unit
-            n_gates = reduce(lambda x, y: x + len(y), compute_unit.sequences, 0)
-            return {'n_gates': n_gates}
-
-        class GateCtrResultsComposer(rottnest_composer.ResultsComposer):
-            '''
-                Results composer that combines gate counter descriptions
-            '''
-            def __init__(self, result_dict=None, n_obj=1, comp_unit=None):
-                super().__init__()
-
-                if result_dict is None:
-                    result_dict = {'n_gates': 0}
-
-                self._obj = result_dict
-
-            def __add__(self, other):
-                res = GateCtrResultsComposer(result_dict={'n_gates': self._obj['n_gates'] + other._obj['n_gates']})
-                res._unit_ids = self._unit_ids + other._unit_ids
-                res._n_obj = self._n_obj + other._n_obj
-                return res
-
-            def __iadd__(self, other):
-                self._unit_ids += other._unit_ids
-                self._n_obj += other._n_obj
-                self._obj['n_gates'] += other._obj['n_gates']
-                return self
-
-            def get_gate_count(self):
-                return self._obj['n_gates']
-
-        composed_counter_arch = build_arch('ComposedGateCounting',
-            build_designer('DummyDesigner', get_mem_bound=lambda s, l : l['mem_bound']),
-            build_composer('GateCtrComposer', results_composer_constructor=lambda s: GateCtrResultsComposer),
-            build_worker('SingleGateCtrWorker', execute_compute_unit=count_gates_composed)
-        )
-
-        architectures._options['ComposedGateCounting'] = composed_counter_arch
-
-        '''
-            Create a selection of useful layouts
-        '''
-        LayoutProxy.add_layout_with_id("std_layout", {'mem_bound': 500})
-        LayoutProxy.add_layout_with_id("low_mem_layout", {'mem_bound': 50})
-
-
     def test_cirq_direct_gate_count(self):
         '''
             Test a worker that parses a circuit (cirq) and directly counts the number of gates
@@ -284,6 +284,8 @@ class TestWorkerCircuitCounting(unittest.TestCase):
         self.assertEqual(res_composer.get_gate_count(), cirq_len(circuit))
 
 
+
+class TestWorkerRzCollection(unittest.TestCase):
     def test_cirq_rz_collection(self):
         layout_id = "std_layout"
 
