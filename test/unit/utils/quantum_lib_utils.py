6277c5c0252501e6a834d6e6463e6f6d9243bc96
Ben Y. <bjy777101@gmail.com>
2025-11-10 15:43:30 +1100

    Added bandaid solution to iterating into composed sub-objects when counting cirq rz

--- test/unit/utils/quantum_lib_utils.py
+++ test/unit/utils/quantum_lib_utils.py
@@ -59,5 +59,43 @@ def cirq_n_rz(circuit):
                 (isinstance(operation.gate, cirq.ZPowGate) and operation.gate.exponent == 0.25)):
                 # NOTE : exponent 0.25 for a ZPow is a T (which maps to rz internally)
                 res[operation.gate.exponent] += 1
+            else:
+                # This is a semi-ok check to see if the operation in question
+                # is a custom (ie. composed) gate vs an internal
+                if hasattr(operation, "_decompose_"):
+                    decomp = cirq.decompose(operation)
+                    if len(decomp) > 1:
+                        res += cirq_n_rz(cirq.Circuit(*decomp))
 
     return res
+
+
+def cirq_circuit_to_gate(circuit, n_qubits):
+    '''
+        Converts a circuit into an equivalent gate class for composition
+    '''
+    class CircuitAsGate(cirq.Gate):
+        def __init__(self):
+            super(CircuitAsGate, self)
+
+        def _num_qubits_(self):
+            return n_qubits
+
+        def _decompose_(self, qubits):
+            qubit_map = dict()
+            qubit_idx = 0
+            for moment in circuit.moments:
+                for operation in moment:
+                    # Greedily allocate qubits from the circuit
+                    # to input qubits
+                    # NOTE : may destroy ordering. For testcases, this doesn't matter
+                    for qb in operation.qubits:
+                        if qb not in qubit_map.keys():
+                            qubit_map[qb] = qubits[qubit_idx]
+                            qubit_idx += 1
+                    yield operation.gate(*map(lambda v: qubit_map[v], operation.qubits))
+
+        def _circuit_diagram_info_(self, args):
+            return ["CircuitGate"] * self.num_qubits()
+
+    return CircuitAsGate
