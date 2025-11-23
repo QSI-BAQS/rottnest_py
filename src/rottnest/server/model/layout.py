86152b8434a737dc4ecbbab7b8319b7286dbd53f
Alan Robertson <alan@qrop.xyz>
2025-11-24 10:31:54 +1100

    Placeholer layout endpoints

---
+++ src/rottnest/server/model/layout.py
@@ -0,0 +1,17 @@
+'''
+    This interface handles the layout controllers 
+'''
+from rottnest.plugins import architectures as singleton
+from rottnest.server.responder import Result
+
+def set_layout(message, **kwargs) -> Result:
+        '''
+            Gets the list of currently loaded layouts
+        '''
+        pass
+
+def run_layout(message, **kwargs) -> Result:
+        '''
+            Gets the list of currently loaded layouts
+        '''
+        pass
