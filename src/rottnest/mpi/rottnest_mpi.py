9e8627183bd622509ca56e80bfbb65bed4ba35cb
Ben Y. <bjy777101@gmail.com>
2025-12-05 14:35:31 +1100

    Added a proper argument parsing system (state machine) to rottnest_mpi

--- src/rottnest/mpi/rottnest_mpi.py
+++ src/rottnest/mpi/rottnest_mpi.py
@@ -18,6 +18,147 @@ from rottnest.process_pool import commands, symbols
 from rottnest.plugins import architectures, executables
 
 
+class RottnestMPIArgs():
+    '''
+        Argument parsing
+    '''
+    STATE_OPEN = object()
+    STATE_LOAD_MODULE = object()
+    STATE_SET_OUTPUT_FILE = object()
+    STATE_CLOSED = object()
+
+
+    def __init__(self):
+        self.output_file = None
+        self.modules_to_load = set()
+
+        self.architecture_name = None
+        self.executable_name = None
+        self.layout_file = None
+
+        self.state = RottnestMPIArgs.STATE_OPEN
+
+        self.state_handlers = {
+            RottnestMPIArgs.STATE_OPEN: self.handle_arg_state_open,
+            RottnestMPIArgs.STATE_LOAD_MODULE: self.handle_arg_state_load_module,
+            RottnestMPIArgs.STATE_SET_OUTPUT_FILE: self.handle_arg_state_set_output_file,
+            # No-Op bound to closed state
+            RottnestMPIArgs.STATE_CLOSED: lambda *a: None
+        }
+
+        self.open_flag_handlers = {
+            "--help": self.help,
+            "-m": lambda s: RottnestMPIArgs.STATE_LOAD_MODULE,
+            "--module": lambda s: RottnestMPIArgs.STATE_LOAD_MODULE,
+            "-o": lambda s: RottnestMPIArgs.STATE_SET_OUTPUT_FILE,
+            "--output_file": lambda s: RottnestMPIArgs.STATE_SET_OUTPUT_FILE
+        }
+
+    def help(self):
+        print(
+'''rottnest_mpi <architecture_name> <executable_name> <layout_file> [OPTIONS...]
+
+USAGE:
+    An MPI-backed standalone rottnest executor.
+    Must be run via mpirun, and intended to be launched as part of a slurm job.
+
+    Given n MPI peers, runs a manager and n-1 clients, with clients doing
+    distributed work.
+
+    <architecture_name>
+        The name of the architecture to use.
+        Tries to load the architecture as a Python module, then falls
+        back to a filepath
+
+    <executable_name>
+        As above, but for an executable.
+
+    <layout_file>
+        A file to load the layout JSON(s) from.
+
+OPTIONS:
+    -m/--module <module path>
+        Provide a module to load as a source of architectures/executables
+        Should be either a Python import path, or a regular file-path to a
+        Python file providing the architectures/executables
+        Can be provided any number of times
+
+    -o/--output_file <file>
+        Provides a file to write the result to (as opposed to dumping it to stdout)'''
+        )
+        return RottnestMPIArgs.STATE_CLOSED
+
+
+    def parse(self, arg):
+        self.state_handlers[self.state](arg)
+
+    def handle_arg_state_open(self, arg):
+        if arg in self.open_flag_handlers.keys():
+            self.state = self.open_flag_handlers[arg](self)
+        elif arg[0] == "-":
+            print(f"Error: unknown flag '{arg}'")
+            self.state = RottnestMPIArgs.STATE_CLOSED
+        else:
+            if self.architecture_name is None:
+                self.architecture_name = arg
+            elif self.executable_name is None:
+                self.executable_name = arg
+            elif self.layout_file is None:
+                self.layout_file = arg
+
+    def handle_arg_state_load_module(self, arg):
+        if arg[0] == "-":
+            print(f"Expected module name, got flag '{arg}'")
+            self.state = RottnestMPIArgs.STATE_CLOSED
+        else:
+            self.modules_to_load.add(arg)
+            self.state = RottnestMPIArgs.STATE_OPEN
+
+    def handle_arg_state_set_output_file(self, arg):
+        if arg[0] == "-":
+            print(f"Expected output file path, got flag '{arg}'")
+            self.state = RottnestMPIArgs.STATE_CLOSED
+        elif self.output_file is not None:
+            print(f"Output file was already set to '{self.output_file}', please set exactly one output file")
+            self.state = RottnestMPIArgs.STATE_CLOSED
+        else:
+            self.output_file = arg
+            self.state = RottnestMPIArgs.STATE_OPEN
+
+    def finalise(self) -> bool:
+        if self.architecture_name is None:
+            print("No architecture name was provided")
+            return False
+        elif self.executable_name is None:
+            print("No executable name was provided")
+            return False
+        elif self.layout_file is None:
+            print("No layout file was provided")
+            return False
+        # We tried to finalise while not in an open state
+        elif self.state is not RottnestMPIArgs.STATE_OPEN:
+            if self.state is RottnestMPIArgs.STATE_LOAD_MODULE:
+                print("Arguments ended while expecting a module name")
+            elif self.state is RottnestMPIArgs.STATE_SET_OUTPUT_FILE:
+                print("Arguments ended while expecting an output file")
+            return False
+
+        return True
+
+
+    @staticmethod
+    def from_args(*args):
+        res = RottnestMPIArgs()
+        for arg in args:
+            res.parse(arg)
+
+        if not res.finalise():
+            return None
+        return res
+
+
+
+
 def root_main(comm, architecture, executable, layouts):
     '''
         Main function for the root process (manager)
@@ -102,6 +243,7 @@ def main(architecture_name, executable_name, layouts):
     executable = executables.get_current_executable()
 
     for layout_id, layout in layouts:
+        pass
         # TODO : Load layouts by id into proxy
 
     if comm.Get_rank() == 0:
@@ -116,46 +258,17 @@ def launch():
     '''
         Handles arg parsing before delegating to main
     '''
-    args = sys.argv[1:]
+    argv = sys.argv[1:]
 
-    if len(args) != 3 or args[0] in ["help", "--help"]:
-        # Ensure only one help message is printed
-        if MPI.COMM_WORLD.Get_rank() == 0:
-            print(
-'''rottnest_mpi <architecture_name> <executable_name> <layout_file> [OPTIONS...]
+    # Silence non-root processes
+    if MPI.COMM_WORLD.Get_rank() != 0:
+        sys.stdout = type("DummyWriter", tuple(), dict(write=lambda *a, **ka: None, flush=lambda *a, **ka: None))
 
-USAGE:
+    # Parse args
+    args = RottnestMPIArgs.from_args(*argv)
 
-    An MPI-backed standalone rottnest executor.
-    Must be run via mpirun, and intended to be launched as part of a slurm job.
-
-    Given n MPI peers, runs a manager and n-1 clients, with clients doing
-    distributed work.
-
-    <architecture_name>
-        The name of the architecture to use.
-        Tries to load the architecture as a Python module, then falls
-        back to a filepath
-
-    <executable_name>
-        As above, but for an executable.
-
-    <layout_file>
-        A file to load the layout JSON(s) from.
-
-OPTIONS:
-    -a <module path>
-        Provide a module to load as a source of architectures
-        Should be either a Python import path, or a regular file-path to a
-        Python file providing the architectures
-
-    -e <module path>
-        As above, but for executable modules
-
-    -o <file>
-        Provides a file to write the result to (as opposed to dumping it to stdout)'''
-            )
-        exit(0)
+    if args is None:
+        exit(1)
 
     # For now, panic if there is only one process
     if MPI.COMM_WORLD.Get_size() == 1:
@@ -164,25 +277,19 @@ OPTIONS:
         exit(1)
 
 
-    # TODO : Proper arg parsing (imports + output)
-
-    arch_name = args[0]
-    exe_name = args[1]
-    layout_file = args[2]
-
     # Open layout file and load layout
     # (currently single layout only, no validation)
     layouts = None
     try:
-        with open(layout_file, 'r') as lf:
+        with open(args.layout_file, 'r') as lf:
             layout_data = lf.read()
 
             layouts = {0: json.loads(layout_data)}
     except Exception as e:
-        print(f"Failed to open file '{layout_file}' : {e}")
+        print(f"Failed to open file '{args.layout_file}' : {e}")
         exit(1)
 
-    main(arch_name, exe_name, layouts)
+    main(args.architecture_name, args.executable_name, layouts)
 
 
 if __name__ == "__main__":
