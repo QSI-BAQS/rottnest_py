561f8da826e750ab8d3ff42be9df30844b437ff2
Ben Y. <bjy777101@gmail.com>
2025-12-09 14:19:12 +1100

    Altered architecture/executable plugin loading to be explicit rather than at-import

--- src/rottnest/plugins/__init__.py
+++ src/rottnest/plugins/__init__.py
@@ -1,5 +1,14 @@
 from .architecture_plugins import ArchitecturePlugins
 from .executable_plugins import ExecutablePlugins
+from .config_loader import _load_default_config
 
-executables = ExecutablePlugins.default_loader()
-architectures = ArchitecturePlugins.default_loader()
+executables = ExecutablePlugins()
+architectures = ArchitecturePlugins()
+
+
+def load_default_architecture_config():
+    _load_default_config(architectures._config_file_name, architectures)
+
+
+def load_default_executable_config():
+    _load_default_config(executables._config_file_name, executables)
