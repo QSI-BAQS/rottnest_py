6c22291eedb3650bb0a907e41c7df5b5a0e50c60
Alan Robertson <alan@qrop.xyz>
2025-12-12 23:10:36 +1100

    View utils

---
+++ src/rottnest/server/view/view_utils.py
@@ -0,0 +1,8 @@
+'''
+    Useful objects for views to return
+    Some of these objects are currently proxies
+     until the front end has set up the types  
+'''
+
+SUCCESS = object()
+ERROR = object()
