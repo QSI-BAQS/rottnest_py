1c65ec2240bcdeac6ca1684934a49f42c01f8dc7
Alan Robertson <alan@qrop.xyz>
2026-01-19 11:38:17 +1100

    Created singleton reference wrapper for the process pool

---
+++ src/rottnest/process_pool/singleton.py
@@ -0,0 +1,49 @@
+'''
+    Singleton reference for process pool object
+'''
+
+from rottnest.process_pool.process_pool import ComputeUnitExecutorPool
+
+class PoolSingletonRef:
+    '''
+        Scoped reference wrapper for the process_pool
+        singleton
+    '''
+
+    process_pool = None
+    
+    @classmethod
+    def get_pool(cls):
+        '''
+            Getter for singleton reference'd class obj
+            Triggers instantiation
+        '''
+        if cls.process_pool is None:
+            cls.process_pool = ComputeUnitExecutorPool() 
+        return cls.process_pool
+
+    @classmethod
+    def instantiate_pool(cls, *args, **kwargs):
+        '''
+            Instantiates and triggers the singleton
+            reference to the process pool
+        '''
+        if cls.process_pool is None: 
+            cls.process_pool = ComputeUnitExecutorPool(*args, **kwargs)
+            return cls.process_pool
+        raise Exception("Pool Already Instantiated")
+
+def get_pool():
+    '''
+        Dispatch for singleton reference 
+    '''
+    return PoolSingletonRef.get_pool()
+
+def instantiate_pool(*args, **kwargs):
+    '''
+        Dispatch for singleton reference
+    '''
+    return PoolSingletonRef.instantiate_pool(
+        *args,
+        **kwargs
+    )
--- test/process_pool_tests/test_process_pool.py
+++ test/process_pool_tests/test_process_pool.py
@@ -14,6 +14,7 @@ from rottnest.compute_units.layout_proxy import LayoutProxy
 from rottnest.preprocessor.architecture import PreprocessorArchitecture
 
 from rottnest.process_pool.process_pool import ComputeUnitExecutorPool
+from rottnest.process_pool.singleton import get_pool
 from rottnest.process_pool.pool_status import PoolStatus
 
 from rottnest.plugins import architectures, executables
@@ -71,6 +72,50 @@ class ProcessPoolTests(unittest.TestCase):
         pool.shutdown()
         print("Triggered shutdown")
         return
+
+
+    def test_process_pool_singleton(self):
+        '''
+            Tests executing the process pool with an Rz counter architecture 
+        '''
+
+        executables.load_modules_from_strings(test_utils.__file__)
+        executables.set_current_executable(SampleExecutable.get_name())
+
+        pool = get_pool() 
+        pool.start()
+
+        # Asserts correctness in here
+        pool.ping_manager()
+       
+        # Synch pool state 
+        pool.synchronise()
+
+        # Setup the pool
+        pool.set_architecture_module(
+            'Rz Counter' 
+        )
+        pool.set_executable(
+            'Sample Executable'
+        )
+        pool.set_executable_params({})
+
+        # Start workers
+        pool.start_workers()
+         
+        # Run the sequence
+        pool.run_sequence([layout_id])
+        
+        while pool.poll() != PoolStatus.FINISHED:
+            # Example busy wait
+            print("Executing...")
+            time.sleep(1)
+
+        # Shutdown the pool
+        pool.shutdown()
+        print("Triggered shutdown")
+        return
+
 
 if __name__ == '__main__':
     obj = ProcessPoolTests()
