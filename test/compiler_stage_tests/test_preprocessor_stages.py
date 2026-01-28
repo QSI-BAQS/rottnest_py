546b146b2d899f6a510dd3fc2fc30adafba33b38
Alan Robertson <alan@qrop.xyz>
2026-01-28 16:12:17 +1100

    First preprocessor stage tests

---
+++ test/compiler_stage_tests/test_preprocessor_stages.py
@@ -0,0 +1,21 @@
+import unittest
+
+from rottnest.compilation_procedures.preprocessor import stage_pandora_store
+from rottnest.compilation_procedures import stage
+
+ 
+class PreprocessorStageTest(unittest.TestCase):
+
+    def test_stage_pandora_tag(self):
+
+        pandora_stage = stage_pandora_store.PandoraLoadStage() 
+        assert pandora_stage.get_tag() == stage_pandora_store.STAGE_TAG 
+
+
+
+
+if __name__ == '__main__':
+    tst = CompilerStageTest()
+    tst.test_stage_default_tag()
+
+    #unittest.main()
