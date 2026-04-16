564d5207ee592e76e463fab907a12b4f4bd7c9f3
Alan Robertson <alan@qrop.xyz>
2026-04-16 11:11:41 +1000

    Updated singleton instance of rz decomposer

--- src/rottnest/rz_decomposer/singleton.py
+++ src/rottnest/rz_decomposer/singleton.py
@@ -5,32 +5,62 @@
 
 from . import gridsynth
 
-# Currently defaults to Gridsynth
-__RZ_DECOMPOSER = None 
+class RzDecomposerSingleton:
+
+    # Currently defaults to Gridsynth
+    __RZ_DECOMPOSER = None 
+
+    @classmethod
+    def get_rz_decomposer(cls):
+        '''
+            Getter for the singleton instance
+        '''
+        if cls.__RZ_DECOMPOSER is None:
+            cls.__RZ_DECOMPOSER = gridsynth.Gridsynth()
+        return cls.__RZ_DECOMPOSER
+
+    @classmethod
+    def set_rz_decomposer(cls, composer):
+        '''
+            Setter for the singleton instance
+        '''
+        cls.__RZ_DECOMPOSER = composer
+
+    @classmethod
+    def get_rz_precision(cls):
+        '''
+            Simple getter for the precision, without loading the decomposer outside of the context
+        '''
+        return get_rz_decomposer().get_precision()
+
+    @classmethod
+    def set_rz_precision(cls, precision):
+        '''
+        '''
+        decomp = get_rz_decomposer()
+        decomp.set_precision(precision)
 
 
 def get_rz_decomposer():
     '''
         Getter for the singleton instance
     '''
-    if __RZ_DECOMPOSER is None:
-        __RZ_DECOMPOSER = gridsynth.Gridsynth()
-    return __RZ_DECOMPOSER
+    return RzDecomposerSingleton.get_rz_decomposer()
 
 def set_rz_decomposer(composer):
     '''
         Setter for the singleton instance
     '''
-    __RZ_DECOMPOSER = composer
+    return RzDecomposerSingleton.set_rz_decomposer(composer)
+
 
 def get_rz_precision():
     '''
         Simple getter for the precision, without loading the decomposer outside of the context
     '''
-    return get_rz_decomposer().get_precision()
+    return RzDecomposerSingleton.get_rz_precision()
+
 
 def set_rz_precision(precision):
-    '''
-    '''
-    decomp = get_rz_decomposer()
-    decomp.set_precision(precision)
+    return RzDecomposerSingleton.set_rz_precision(precision)
+
