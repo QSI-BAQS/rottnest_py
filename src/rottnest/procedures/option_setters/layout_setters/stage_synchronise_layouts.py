83dea6cab7340ad01f7208647c8959fc0d7a7952
Ben Y. <bjy777101@gmail.com>
2026-04-16 16:21:43 +1000

    Updated layout proxy and layout sync procedure to refresh memory bounds
    Membounds were lingering from prior architectures, fixed with an explicit refresh

--- src/rottnest/procedures/option_setters/layout_setters/stage_synchronise_layouts.py
+++ src/rottnest/procedures/option_setters/layout_setters/stage_synchronise_layouts.py
@@ -16,24 +16,27 @@ class SynchroniseLayoutsStage(stage.RottnestCompilerStage):
         '''
         self._complete = False
         self._layouts = layouts
-        
+
         if dependencies is None:
             dependencies = []
- 
+
         super().__init__(
-            tag=tag, 
+            tag=tag,
             dependencies=dependencies,
             asynchronous=False
         )
 
     def execute(self, compiler_environment):
         '''
-            Swaps the current rottnest architecture 
+            Swaps the current rottnest architecture
         '''
         # This bound is reasonably performant
         for layout_id, layout_json in self._layouts:
             LayoutProxy.add_layout_with_id(
                 layout_id, layout_json
             )
+            LayoutProxy.refresh_mem_bound_by_id(layout_id)
+
+        LayoutProxy.refresh_all_mem_bounds()
 
         self._complete = True
