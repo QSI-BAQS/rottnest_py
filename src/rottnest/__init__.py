fb52535bd32fc4709b173da8c0569219db07a9e0
Alan Robertson <alan@qrop.xyz>
2026-04-21 15:27:32 +1000

    Environment instantiation on import

--- src/rottnest/__init__.py
+++ src/rottnest/__init__.py
@@ -1,1 +1,1 @@
-
+from . import environment
--- src/rottnest/environment.py
+++ src/rottnest/environment.py
@@ -1,8 +1,10 @@
 import os
 
-class ServerEnvironmentSettings:
+from rottnest.config import OMP_NUM_THREADS, MPLBACKEND
+
+class EnvironmentSettings:
     '''
-       ServerEnviromentSettings
+       EnviromentSettings
 
        Allows for some sensible defaults that can be set for the
        application
@@ -10,31 +12,38 @@ class ServerEnvironmentSettings:
 
     _instance = None
     
-    def __init__(self, numpy_processes=1, mpl_backend="Agg"):
+    def __init__(
+            self,
+            numpy_processes=None,
+            mpl_backend=None
+        ):
         '''
            Initialises the server environment settings with defaults
            unless it is specified to be overloaded 
         '''
         self.environment_set = False
+        if numpy_processes is None:
+            numpy_processes = OMP_NUM_THREADS
+        if mpl_backend is None:
+            mpl_backend = MPLBACKEND
+
         self.numpy_processes = numpy_processes
         self.mpl_backend = mpl_backend
         self.environment_dict = {
             "OMP_NUM_THREADS": f"{numpy_processes}",
             "MPLBACKEND" : f"{mpl_backend}"
         }
+        self._set_environment_variables()
 
     @classmethod
     def get_instance(cls):
         '''
            Gets the singleton instance for the environment variables 
         '''
-        current_instance = ServerEnvironmentSettings._instance
-        if current_instance is None:
-            current_instance = ServerEnvironmentSettings()
-            current_instance._set_environment_variables()   
-            ServerEnvironmentSettings._instance = current_instance
-
-        return current_instance
+        
+        if cls._instance is None:
+            cls._instance = EnvironmentSettings()
+        return cls._instance 
 
 
     def _set_environment_variables(self):
@@ -46,4 +55,4 @@ class ServerEnvironmentSettings:
 
 
 # Immediately invokes the get_instance and processes the environment variables
-__server_env_instance = ServerEnvironmentSettings.get_instance()
+__env_instance = EnvironmentSettings.get_instance()
