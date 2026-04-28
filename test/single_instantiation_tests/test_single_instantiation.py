36289d76ba20ed83e3379d759f7104d70419ccf0
Alan Robertson <alan@qrop.xyz>
2026-04-28 10:59:29 +1000

    Adding tests for single instantiation

---
+++ test/single_instantiation_tests/test_single_instantiation.py
@@ -0,0 +1,89 @@
+import unittest
+
+from rottnest.process_pool.single_instantiation import (
+    SingleInstantiation,
+    InstantiatingBlockedObjectException, 
+    BlockingInstantiatedObjectException,
+    MultipleInstantiationException)
+
+class TestSingleInstantiation(unittest.TestCase):
+
+    def class_builder(self):
+        '''
+            Builds dummy classes for testing
+        '''
+
+        class Obj(SingleInstantiation):
+            def __init__(self):
+                ...
+        return Obj
+
+    def test_single_init(self):
+        '''
+            Tests that only one init may occur
+        '''
+        Obj = self.class_builder()
+        
+        a = Obj()
+         
+        # Second init should fail
+        try:
+            _ = Obj()
+            # Should not reach
+            assert False
+        except MultipleInstantiationException:
+            pass
+
+
+    def test_class_scoping(self):
+        '''
+            Ensuring that the single init restriction is
+            correctly per-class
+        '''
+        Obj_a = self.class_builder()
+        Obj_b = self.class_builder()
+        Obj_c = self.class_builder()
+
+        # Compared to the previous test, none of these
+        # should fail
+        a = Obj_a()
+        b = Obj_b()
+        c = Obj_c()
+
+        # Second init should fail for each
+        for Obj in [Obj_a, Obj_b, Obj_c]:
+            try:
+                _ = Obj()
+                # Should not reach
+                assert False
+            except MultipleInstantiationException:
+                pass
+
+    def test_block_then_init(self):
+        Obj = self.class_builder()
+        
+        SingleInstantiation.block_instantiation(Obj)
+ 
+        # Init should fail
+        try:
+            a = Obj()
+            # Should not reach
+            assert False
+        except InstantiatingBlockedObjectException:
+            pass
+
+    def test_init_then_block(self):
+        Obj = self.class_builder()
+        
+        a = Obj()
+ 
+        # Init should fail
+        try:
+            SingleInstantiation.block_instantiation(Obj)
+            # Should not reach
+            assert False
+        except BlockingInstantiatedObjectException:
+            pass
+
+if __name__ == '__main__':
+    unittest.main()
