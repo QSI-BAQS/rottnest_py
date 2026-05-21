c6291fb44ac3944994e76d1c671b5728a3dbc866
Ben Y. <bjy777101@gmail.com>
2026-05-21 12:37:07 +1000

    Added test_layouts test for changing architecture and ensuring that still-loaded layouts (such as those for the preprocessor) have their num registers correctly updated

--- test/unit/test_layouts.py
+++ test/unit/test_layouts.py
@@ -6,6 +6,28 @@ import random
 
 from rottnest.compute_units.layout_proxy import LayoutProxy
 
+from rottnest.plugins import architectures
+from rottnest.architecture_interface.rottnest_architecture import RottnestArchitecture
+
+
+def make_dummy_arch(mem_fn):
+    class DummyDesigner():
+        @staticmethod
+        def get_mem_bound(layout):
+            return mem_fn(layout)
+
+    class DummyArchitecture(RottnestArchitecture):
+        @staticmethod
+        def get_name():
+            return "DummyArch"
+
+        @staticmethod
+        def designer(*a, **ka):
+            return DummyDesigner
+
+    return DummyArchitecture
+
+
 
 class LayoutProxyTests(unittest.TestCase):
 
@@ -61,6 +83,48 @@ class LayoutProxyTests(unittest.TestCase):
         for idx, mem in layout_vals.items():
             layout = LayoutProxy.get_layout(idx)
             assert mem == layout['mem_bound']
+
+
+    def test_add_layout(self):
+        '''
+            Sanity check to ensure that add_layout w/out specified
+            id does add with distinct ids
+        '''
+        LayoutProxy.flush()
+        layout_vals = {}
+        idx = 0
+        for _ in range(100):
+            layout_vals[idx] = random.randint(100, 1000)
+            idx += 1
+
+        for idx, mem in layout_vals.items():
+            LayoutProxy.add_layout(
+                self.generate_layout_obj(mem)
+            )
+
+        self.assertEqual(len(LayoutProxy.saved_layouts), 100)
+
+
+    def test_layout_architecture_mem_bound(self):
+        arch_1 = make_dummy_arch(lambda l: l['mem_bound'])
+        arch_2 = make_dummy_arch(lambda l: 2 * l['mem_bound'])
+
+        architectures._force_set_current_architecture(arch_1)
+
+        LayoutProxy.flush()
+
+        LayoutProxy.add_layout_with_id(0, self.generate_layout_obj(10))
+
+        layout = LayoutProxy(0)
+        self.assertEqual(layout.num_registers, 10)
+
+        # switch arch and force refresh
+        architectures._force_set_current_architecture(arch_2)
+
+        LayoutProxy.force_proxy_refresh()
+
+        self.assertEqual(layout.num_registers, 20)
+
 
 
 if __name__ == '__main__':
