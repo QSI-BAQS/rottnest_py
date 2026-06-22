a45e9573975f0d8271ab45105fa9ca8a320f1446
Alan Robertson <alan@qrop.xyz>
2026-06-22 15:02:25 +1000

    More linting error budgets

--- src/rottnest/error_budgets/singleton.py
+++ src/rottnest/error_budgets/singleton.py
@@ -6,17 +6,20 @@ from .even_distribution import EvenDistribution
 
 error_budget_object = EvenDistribution()
 
+
 def get_error_budget() -> "ErrorBudget":
     '''
         Getter for the singleton instance
     '''
     return error_budget_object
 
+
 def set_p_physical(p_phys):
     '''
     Dipatch to singleton setter
     '''
     error_budget_object.set_p_physical(p_phys)
+
 
 def set_target_error(err: float):
     '''
