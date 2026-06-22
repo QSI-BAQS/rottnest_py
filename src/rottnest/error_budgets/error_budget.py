05c2742cc00731a7db0dc38b367be2de09909864
Alan Robertson <alan@qrop.xyz>
2026-06-22 12:15:05 +1000

    Error Budget linting

--- src/rottnest/error_budgets/error_budget.py
+++ src/rottnest/error_budgets/error_budget.py
@@ -12,7 +12,7 @@ class ErrorBudget(abc.ABC):
             self,
             p_physical: float,
             target_error: float
-        ): 
+        ):
         '''
             Error budget base class
         '''
@@ -20,19 +20,41 @@ class ErrorBudget(abc.ABC):
         self._p_physical = p_physical
         self._target_error = target_error
 
+    @classmethod
+    def get_model_name(cls):
+        '''
+            Getter for a model name to expose to an API
+        '''
+
+    @classmethod
+    def get_base_model_parameters(cls):
+        '''
+            Simple getter for some base parameters
+        '''
+        return {
+            'p_physical': (float, None),
+            'target_error': (float, None)
+        }
+
+    @classmethod
+    def get_model_parameters(cls):
+        '''
+            Getter for model parameters
+        '''
+
     def get_p_physical(self) -> float:
         '''
             Get the physical error rate
         '''
         return self._p_physical
-  
+
     def set_p_physical(self, p_physical):
         '''
             Setter for the physical error rate
         '''
         self._p_physical = p_physical
- 
-    def get_target_error(self) -> float: 
+
+    def get_target_error(self) -> float:
         '''
             Gets the target erro
         '''
@@ -50,16 +72,21 @@ class ErrorBudget(abc.ABC):
             budget targets
         '''
         # TODO - decide a nice implementation
- 
-    @abc.abstractclassmethod
+
+    @abc.abstractmethod
     def get_rz_precision_budget(self) -> float:
-        ...
+        '''
+            Base Rz precision budget getter
+        '''
 
-    @abc.abstractclassmethod
+    @abc.abstractmethod
     def get_t_infidelity_budget(self) -> float:
-        ...
-               
-    @abc.abstractclassmethod
-    def get_space_time_budget(self) -> float:
-        ...
+        '''
+            Base T infidelity budget getter
+        '''
 
+    @abc.abstractmethod
+    def get_space_time_budget(self) -> float:
+        '''
+        Base space-time budget getter
+        '''
--- src/rottnest/error_budgets/even_distribution.py
+++ src/rottnest/error_budgets/even_distribution.py
@@ -9,29 +9,57 @@ class EvenDistribution(ErrorBudget):
         Simple even error distribution model
     '''
 
-    def __init__(self, 
+    MODEL_NAME = "Bounded Sum"
+    DEFAULT_COEFF = 4
+
+    def __init__(self,
         p_physical=1e-3,
-        target_error = 1e-2
+        target_error = 1e-2,
+        coeff = None
     ):
         '''
             Constructor for an even distribution
         '''
+        if coeff is None:
+            coeff = self.DEFAULT_COEFF
+        assert coeff >= 3
+
+        self.coeff = coeff
+
         super().__init__(p_physical, target_error)
+
+    @classmethod
+    def get_model_name(cls):
+        '''
+            Getter
+        '''
+        return cls.MODEL_NAME
+
+    @classmethod
+    def get_model_parameters(cls):
+        '''
+            Getter for model parameters
+        '''
+        return (cls.get_base_model_parameters()
+        | {
+            'coeff': (float, cls.DEFAULT_COEFF)
+          }
+        )
 
     def get_rz_precision_budget(self) -> float:
         '''
             Sets an even split
         '''
-        return self.get_target_error() / 4
+        return self.get_target_error() / self.coeff
 
     def get_t_infidelity_budget(self) -> float:
         '''
             Sets an even split
         '''
-        return self.get_target_error() / 4
-               
+        return self.get_target_error() / self.coeff
+
     def get_space_time_budget(self) -> float:
         '''
             Sets an even split
         '''
-        return self.get_target_error() / 4
+        return self.get_target_error() / self.coeff
